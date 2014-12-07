8201fcebecdd956792c9f895c882d78a0057747e
Thomas Lehner <thomas.lehner@justremotephone.com>
2014-12-07 19:52:54 +0100

    media-legacy: Build only supported modules for qsd8k.
    After project pathmap update this is needed. See:
    
    https://github.com/szezso/android_hardware_qcom_media-legacy/commits/cm-12.0
    @9e9435bd140696a33367b15cea74b60d5856771c
    
    May bee some or all changes of this project should be merged too.
    
    Change-Id: Iacf3199cc8fa03155539d43baf9004508e7d1a97

--- Android.mk
+++ Android.mk
@@ -1,5 +1,13 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
-include $(call all-subdir-makefiles)
+QCOM_MEDIA_ROOT := $(call my-dir)
+
+ifeq ($(filter qsd8k,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
+endif
+
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
 endif
